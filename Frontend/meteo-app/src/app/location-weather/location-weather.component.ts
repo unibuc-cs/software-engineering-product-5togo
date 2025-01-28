36b3971dfa91bcee1933aef4a3cc8c1e5392df56
Hutanu Eusebiu-Ciprian <eusebiuciprian.hutanu03@gmail.com>
2025-01-28 19:58:09 +0200

    Initial Angular Project
    Adds the generated Angular files.

---
+++ Frontend/meteo-app/src/app/location-weather/location-weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-weather',
+  imports: [],
+  templateUrl: './location-weather.component.html',
+  styleUrl: './location-weather.component.css'
+})
+export class LocationWeatherComponent {
+
+}
