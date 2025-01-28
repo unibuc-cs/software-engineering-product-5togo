41a91e6ec3ca47c003d3a5ffa954818c7fd4caf2
Hutanu Eusebiu-Ciprian <eusebiuciprian.hutanu03@gmail.com>
2025-01-28 21:41:59 +0200

    Weather Panel
    Changed icons location to be able to display them on the weather panel. I also did some minor work on the panel itself, more to come tomorrow.

--- Frontend/meteo-app/src/app/app.routes.ts
+++ Frontend/meteo-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LocationWeatherComponent} from './location-weather/location-weather.component';
+import {RouterModule, Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'weather', component: LocationWeatherComponent },
+  { path: '', redirectTo: '/weather', pathMatch: 'full' }]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- Frontend/meteo-app/src/app/location-weather/location-weather.component.ts
+++ Frontend/meteo-app/src/app/location-weather/location-weather.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './location-weather.component.css'
 })
 export class LocationWeatherComponent {
+  isNavActive = false;
+  weatherData = {
+    umezeala: 56,
+    fenomen_e: "indisponibil",
+    zapada: "indisponibil",
+    actualizat: "28-01-2025 ora 14:00",
+    nebulozitate: "cer partial noros",
+    presiunetext: "995.3 mb, in scadere",
+    icon: "2",
+    nume: "ADAMCLISI",
+    tempe: "15.5",
+    vant: "3.0 m/s, directia : S",
+    tempapa: "indisponibil"
+  };
 
+  toggleNav() {
+    this.isNavActive = !this.isNavActive;
+  }
 }
+
