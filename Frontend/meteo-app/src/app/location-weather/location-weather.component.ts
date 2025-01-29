4f748d22845afc8311991308f35851dca46cbe12
Hutanu Eusebiu-Ciprian <eusebiuciprian.hutanu03@gmail.com>
2025-01-29 20:24:57 +0200

    Visual improvements
    Completed the design for the weather page, added the side navbar with the search bar, TODO: API calls.

--- Frontend/meteo-app/src/app/location-weather/location-weather.component.ts
+++ Frontend/meteo-app/src/app/location-weather/location-weather.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-location-weather',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './location-weather.component.html',
   styleUrl: './location-weather.component.css'
 })
+
 export class LocationWeatherComponent {
   isNavActive = false;
   weatherData = {
@@ -15,15 +18,30 @@ export class LocationWeatherComponent {
     actualizat: "28-01-2025 ora 14:00",
     nebulozitate: "cer partial noros",
     presiunetext: "995.3 mb, in scadere",
-    icon: "2",
+    icon: "51",
     nume: "ADAMCLISI",
     tempe: "15.5",
     vant: "3.0 m/s, directia : S",
     tempapa: "indisponibil"
   };
+  searchQuery = '';
+  bookmarkedLocations = [
+    { name: 'Bucharest', temperature: 18 },
+    { name: 'Cluj-Napoca', temperature: 15 },
+  ];
+  userName = 'John Doe';
 
   toggleNav() {
     this.isNavActive = !this.isNavActive;
   }
+
+  searchLocation() {
+    console.log('Searching for:', this.searchQuery);
+  }
+
+  logout() {
+    console.log('User logged out');
+  }
+
 }
 
