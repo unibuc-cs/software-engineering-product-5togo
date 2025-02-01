d3094428bdf90068906ee2a608ec4a9daefa1c18
e <scaunasu_anne.marie@yahoo.com>
2025-02-02 01:34:22 +0200

    Completed tests for WeatherController + Partial tests for AuthController

---
+++ WeatherDashboard.Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using AuthService.Controllers;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WeatherDashboard.Tests.Controllers
+{
+    internal class AuthControllerTests
+    {
+        private readonly UserManager<IdentityUser> _userManagerMock;
+        private readonly SignInManager<IdentityUser> _signInManagerMock;
+        private readonly IConfiguration _configurationMock;
+        private readonly AuthController SUT;
+
+        public AuthControllerTests() 
+        {
+            _userManagerMock = A.Fake<UserManager<IdentityUser>>();
+            _signInManagerMock = A.Fake<SignInManager<IdentityUser>>();
+            _configurationMock = A.Fake<IConfiguration>();
+            SUT = new AuthController(_userManagerMock, _signInManagerMock, _configurationMock);
+        }
+
+        [Fact]
+        public async Task AuthController_Register_WhenRegistrationSucceeds()
+        {
+            // Arrange
+            var user = new IdentityUser();
+            A.CallTo(() => _userManagerMock.CreateAsync(user, A<string>.Ignored)).Returns(Task.FromResult(IdentityResult.Success));
+
+            //Act
+            var result = SUT.Register(new AuthService.Models.RegisterModel());
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var resultMessage = okResult.Value as string;
+            resultMessage.Should().Be("User registered successfully");
+        }
+
+        [Fact]
+        public async Task AuthController_Register_WhenRegistrationFails()
+        {
+            // Arrange
+            var user = new IdentityUser();
+            A.CallTo(() => _userManagerMock.CreateAsync(user, A<string>.Ignored)).Returns(Task.FromResult(IdentityResult.Success));
+
+            //Act
+            var result = SUT.Register(new AuthService.Models.RegisterModel());
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var resultMessage = okResult.Value as string;
+            resultMessage.Should().Be("User registered successfully");
+        }
+    }
+}
---
+++ WeatherDashboard.Tests/Controllers/WeatherControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using WeatherDashboard.Controllers;
+using WeatherDashboard.Services.WeatherService;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using WeatherDashboard.Models;
+
+namespace WeatherDashboard.Tests.Controllers
+{
+    public class WeatherControllerTests
+    {
+        private readonly IWeatherService _weatherServiceMock;
+        private readonly WeatherController SUT;
+        
+        public WeatherControllerTests() 
+        {
+            _weatherServiceMock = A.Fake<IWeatherService>();
+            SUT = new WeatherController(_weatherServiceMock);
+        }
+
+        [Fact]
+        public async Task WeatherController_StoreWeatherDataFromApi_WhenServiceSucceeds()
+        {
+            // Arrange
+            A.CallTo(() => _weatherServiceMock.StoreWeatherDataFromApiAsync(A<string>.Ignored)).Returns(Task.CompletedTask);
+
+            // Act
+            IActionResult result =await SUT.StoreWeatherDataFromApi();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            okResult.Value.Should().Be("Weather data successfully stored in the database.");
+
+        }
+
+        [Fact]
+        public async Task WeatherController_StoreWeatherDataFromApi_WhenServiceThrowsException()
+        {
+            // Arrange
+            A.CallTo(() => _weatherServiceMock.StoreWeatherDataFromApiAsync(A<string>.Ignored)).ThrowsAsync(new Exception("Service error"));
+
+            // Act
+            IActionResult result = await SUT.StoreWeatherDataFromApi();
+
+            // Assert
+            var statusCode = Assert.IsType<ObjectResult>(result);
+            var errorMessage = statusCode.Value as string;
+            statusCode.StatusCode.Should().Be(500);
+            errorMessage.Should().Contain("An error occurred:");
+
+        }
+
+        [Fact]
+        public async Task WeatherController_GetAllWeather_WhenServiceSucceeds()
+        {
+            // Arrange
+            var weatherDataMock = A.Fake<IEnumerable<WeatherData>>();
+            A.CallTo(() => _weatherServiceMock.GetAllWeatherAsync()).Returns(weatherDataMock);
+            // Act
+            var result = await SUT.GetAllWeather();
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            okResult.Value.Should().Be(weatherDataMock);
+        }
+
+        [Fact]
+        public async Task WeatherController_GetWeatherForLocation_WhenServiceSucceeds()
+        {
+            // Arrange
+            var weatherDataMock = A.Fake<WeatherData>();
+            A.CallTo(() => _weatherServiceMock.GetWeatherByLocationAsync(A<string>.Ignored)).Returns(weatherDataMock);
+            // Act
+            var result = await SUT.GetAllWeather();
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
